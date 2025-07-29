ca4f6b09256fe468c5de00161662fa71a1017b72
Carlos Elias <carloseliasf146@gmail.com>
2025-07-29 17:13:09 -0300

    upload questoes

---
+++ musicas/musicas.h
@@ -0,0 +1,12 @@
+#ifndef MUSICAS_H
+#define MUSICAS_H
+
+#include <stdint.h>
+
+#define AUDIO_DATA_LEN_1 92160
+#define AUDIO_DATA_LEN_2 118800
+
+extern const uint8_t carlos_miguel[AUDIO_DATA_LEN_2];
+extern const uint8_t specialz[AUDIO_DATA_LEN_1];
+
+#endif // MUSICA_H
